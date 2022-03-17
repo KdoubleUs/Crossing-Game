4db2e6aff05848fe84168bbf89d884acf17da909
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-03-17 09:35:19 -0400

    fix alert repeating bug

--- script.js
+++ script.js
@@ -2,88 +2,60 @@ let ball = document.querySelector(".ball");
 movePosition = 50;
 window.addEventListener("load", () => {
   ball.style.left = 0;
-  //   ball.style.right = 0;
   ball.style.bottom = 0;
 });
 
 window.addEventListener("keydown", function (e) {
-  //   e.preventDefault();
+  // e.preventDefault();
   switch (e.key) {
     case "ArrowLeft":
       ball.style.left = parseInt(ball.style.left) - movePosition + "px";
       checkCollision();
       checkLoser();
+      safeBlock();
       break;
     case "ArrowRight":
       ball.style.left = parseInt(ball.style.left) + movePosition + "px";
       checkCollision();
       checkLoser();
-
+      safeBlock();
       break;
     case "ArrowDown":
       ball.style.bottom = parseInt(ball.style.bottom) - movePosition + "px";
 
       checkCollision();
       checkLoser();
-
+      safeBlock();
       break;
     case "ArrowUp":
       ball.style.bottom = parseInt(ball.style.bottom) + movePosition + "px";
       checkCollision();
       checkLoser();
-
+      safeBlock();
       break;
   }
 });
 
-//check for collision detection
-// let blockPosition = ball.getBoundingClientRect();
-// function checkSpot() {
-//   let blockPosition = ball.getBoundingClientRect();
-//   console.log(blockPosition);
-//   console.log(blockPosition.top);
-//   console.log(blockPosition.bottom);
-//   console.log(blockPosition.left);
-//   console.log(blockPosition.right);
-// }
-// checkSpot();
 //check for block position
 let block1 = document.getElementById("b4").getBoundingClientRect();
 
-// function checkBlockSpot() {
-//   let block1 = document.getElementById("b4").getBoundingClientRect();
-//   console.log(block1);
-//   console.log(block1.top);
-//   console.log(block1.bottom);
-//   console.log(block1.left);
-//   console.log(block1.right);
-// }
-
-// checkBlockSpot();
-
 //checking for collision
 let testBlock = document.querySelectorAll(".block");
-let safeZone = document.querySelector(".safe");
+// let safeZone = document.querySelector(".safe");
+// let yellowBall = ball.getBoundingClientRect();
 
 function checkCollision() {
   let hearts = document.querySelector(".heart");
-
   let yellowBall = ball.getBoundingClientRect();
-  let safeArena = safeZone.getBoundingClientRect();
+  // let safeArena = safeZone.getBoundingClientRect();
 
   testBlock.forEach(testBlock => {
-    // let blocky = document.getElementById("b4").
     let blocky = testBlock.getBoundingClientRect();
-    // let collision = false;
     if (
       blocky.x < yellowBall.x + blocky.width &&
       blocky.x + blocky.width > yellowBall.x &&
       blocky.y < yellowBall.y + yellowBall.height &&
       blocky.height + blocky.y > yellowBall.y
-      // yellowBall.x < yellowBall.width + blocky.x &&
-      // yellowBall.x + yellowBall.width > blocky.x &&
-      // yellowBall.y < blocky.y + blocky.height &&
-      // yellowBall.height + yellowBall.y > blocky.y
     ) {
       console.log(blocky.x);
       console.log(blocky.y);
@@ -101,47 +73,43 @@ function checkCollision() {
 
       console.log(`collision occured`);
       hearts.remove();
-      // collision = true;
-      // alert(`you just got smacked by the block start again`);
-      // loseLife();
-      checkLoser();
-      resetPosition();
-    }
-    if (
-      safeArena.x < yellowBall.x + safeArena.width &&
-      safeArena.x + safeArena.width > yellowBall.x &&
-      safeArena.y < yellowBall.y + yellowBall.height &&
-      safeArena.height + safeArena.y > yellowBall.y
-      // yellowBall.x < yellowBall.width + safeArena.x &&
-      // yellowBall.x + yellowBall.width > safeArena.x &&
-      // yellowBall.y < safeArena.y + safeArena.height &&
-      // yellowBall.height + yellowBall.y > safeArena.y
-    ) {
-      console.log(`you are safe`);
-      alert(`Congratulation you are safe`);
+
       resetPosition();
     }
   });
 }
 
+//function check for safe zone
+function safeBlock() {
+  let yellowBalls = ball.getBoundingClientRect();
+  let safeZone = document.querySelector(".safe");
+
+  let safeArena = safeZone.getBoundingClientRect();
+
+  if (
+    safeArena.x < yellowBalls.x + safeArena.width &&
+    safeArena.x + safeArena.width > yellowBalls.x &&
+    safeArena.y < yellowBalls.y + yellowBalls.height &&
+    safeArena.height + safeArena.y > yellowBalls.y
+  ) {
+    console.log(`you are safe`);
+    alert(`Congratulation you are safe`);
+    resetPosition();
+  }
+}
+
+//change the yellow ball back to the original spot
 function resetPosition() {
   ball.style.left = 0;
   ball.style.bottom = 0;
 }
 
 function checkLoser() {
-  // let hearts = document.querySelector(".heart");
   let totalHearts = document.querySelectorAll(".heart");
-  // if (totalHearts.length !== 0) {
-  //   hearts.remove();
-  // } else
   if (totalHearts.length === 0) {
     alert(`you lost. Restart the game!`);
   }
-  // console.log(totalHearts.length);
 }
-// loseLife();
-// checkLoser();
 
 //reset the game from the button
 let restart = document.getElementById("reset");
